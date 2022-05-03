88557c04566a19bb681567f7c9dd7f669a5dbe5d
sathishramalingam <102944190+sathishramalingam@users.noreply.github.com>
2022-05-03 13:13:34 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIAVFBR3CHWJZY2S3UV"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "ap-south-1"
+  }
